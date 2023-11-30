d7ad9d9bf7879412f52d8704b323ac300b261b6a
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-30 13:26:18 +0500

    diff btw public & static

--- practice/src/App.java
+++ practice/src/App.java
@@ -1,18 +1,20 @@
 public class App {
-    static void myStaticClass(){
+    static void myStaticClass() {
         System.out.println("this is static method");
     }
 
-    public void myPublicMethod(){
+    public void myPublicMethod() {
         System.out.println("this is a public method");
     }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Tv Tv1 = new Tv();
+        Tv Tv1;
+        Tv1 = new Tv();
         Tv1.turnOn();
         Tv1.setChannel(21);
         Tv1.setVol(30);
-        // myStaticClass();
+        myStaticClass();
         App class1 = new App();
         class1.myPublicMethod();
 
